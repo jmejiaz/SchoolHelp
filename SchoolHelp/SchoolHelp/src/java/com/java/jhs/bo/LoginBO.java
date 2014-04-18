856b8605ec7a678cfac9187f3b4efd7c433ddb10
jmejiaz <jmejiaz@eafit.edu.co>
2014-04-17 19:42:23 -0500

    try again

---
+++ SchoolHelp/SchoolHelp/src/java/com/java/jhs/bo/LoginBO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.jhs.bo;
+
+import com.java.jsf.bean.BeanLogin;
+
+/**
+ *
+ * @author SergioRios
+ */
+public interface LoginBO {
+    void validaLogin(BeanLogin obj);
+}
