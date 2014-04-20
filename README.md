e0bf81035a6a2983e8f6cdc9deca3ccb10f3f348
jmejiaz <jmejiaz@eafit.edu.co>
2014-04-19 22:12:12 -0500

    cleaning

--- README.md
+++
@@ -1,3 +0,0 @@
-SchoolHelp
-==========
-This is an educational software, a place where the people post academic questions and yo can help them.
--- SchoolHelp/src/java/com/java/jdbc/dao/IfaceLogin.java
+++
@@ -1,8 +0,0 @@
-package com.java.jdbc.dao;
-
-import com.java.jhs.persistence.Login;
-
-public interface IfaceLogin {
-    Login validaLogin(Login obj);
-}
-
